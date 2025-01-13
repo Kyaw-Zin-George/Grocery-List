361cae6f8cde538162c166c5eb9dce7ead3dd6e1
Kyaw-Zin-George <ktz1695@gmail.com>
2025-01-13 23:02:23 +0000

    Adding 3 new features: adding items, updating items and deleting ones via swipeactions and writing the same line of code on multiple cursors using some short cuts

--- Grocery List/ContentView.swift
+++ Grocery List/ContentView.swift
@@ -12,6 +12,12 @@ struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items : [Item]
     
+    func addEssentialFood(){
+        modelContext.insert(Item(title: "Bakery", isCompleted: true))
+        modelContext.insert(Item(title: "Fruits", isCompleted: false))
+        modelContext.insert(Item(title: "Meat", isCompleted: .random()))
+        modelContext.insert(Item(title: "Vegetables", isCompleted: true))
+    }
     
     var body: some View {
         NavigationStack {
@@ -23,12 +29,38 @@ struct ContentView: View {
                         .foregroundStyle(item.isCompleted == false ? Color.primary: Color.red)
                         .strikethrough(item.isCompleted)
                         .italic(item.isCompleted)
+                    //delete action
+                        .swipeActions {
+                            Button(role: .destructive){
+                                withAnimation{
+                                    modelContext.delete(item)
+                                }
+                            }label:{
+                                Label("Delete", systemImage: "trash")
+                            }
+                        }
+                        .swipeActions(edge: .leading) {
+                            Button("Done",systemImage: item.isCompleted  == false  ? "checkmark.circle" : "x.circle"){
+                                item.isCompleted.toggle()
+                            }.tint(item.isCompleted == false ? .green : .accentColor)
+                        }
                 }
             }
             .navigationTitle("Grocery List")
             .overlay{
                 if items.isEmpty{
                     ContentUnavailableView("Empty Cart", systemImage: "cart.circle", description:Text("Add Some Items on the shopping list"))
+                }
+            }
+            .toolbar{
+                if items.isEmpty{
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button{
+                            addEssentialFood()
+                        }label:{
+                            Label("Essentials", systemImage: "carrot")
+                        }
+                    }
                 }
             }
         }
@@ -39,7 +71,8 @@ struct ContentView: View {
     let sampleData :[Item] = [
         Item(title: "Bakery", isCompleted: true),
         Item(title: "Fruits", isCompleted: false),
-        Item(title: "Meat", isCompleted: .random())
+        Item(title: "Meat", isCompleted: .random()),
+        Item(title: "Vegetables", isCompleted: .random())
     ]
     let container = try! ModelContainer(for:Item.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
     for item in sampleData{
